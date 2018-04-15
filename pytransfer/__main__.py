9680b6392f9f201f9f62ddfe87387fe965308303
Patrik Janoušek <patrikjanousek97@gmail.com>
2018-04-15 17:56:38 +0200

    First working version

--- pytransfer/__main__.py
+++ pytransfer/__main__.py
@@ -1,8 +1,42 @@
+import argparse
+import sys
+from collections import deque
+
+from pytransfer.utils import upload_file
+
+files_queue = deque()
+
+
+def get_args(args):
+    arg = argparse.ArgumentParser(description="App that simplifies usage of https://transfer.sh")
+    arg.add_argument(
+        'filename',
+        nargs='*',
+        default=(),
+        help='Name of file you want to upload'
+    )
+
+    return arg.parse_args(args)
+
+
+def process_queue():
+    while True:
+        try:
+            print(upload_file(files_queue.popleft()))
+        except IndexError:
+            break
+
+
 def main():
     """
     Main entrypoint
     """
-    pass
+    args = get_args(sys.argv[1:])
+
+    for filename in args.filename:
+        files_queue.append(filename)
+
+    process_queue()
 
 
 if __name__ == '__main__':
